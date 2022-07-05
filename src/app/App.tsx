d41e23b86dc215af4494c3f8a7cb61a9821f0dab
quoclong01 <ngodacquoclong@gmail.com>
2022-07-05 09:00:35 +0700

    add local for user and fix scroll top

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../stylesheets/styles.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { MyGlobalContext } from "./shared/contexts/cart.context";
 import { ICart } from "./shared/interfaces/cart";
 import { Home, Cart, Register } from "./pages/index";
@@ -9,30 +9,40 @@ import { getData, setData } from './shared/common/common';
 import { IForm } from './shared/interfaces/form';
 
 function App() {
+  const location = useLocation();
   const [cart, setCart] = useState<ICart[]>(getData('cart', []));
-  const [user, setUser] = useState<IForm>({email: '', password: '', confirmPassword: ''});
+  const [user, setUser] = useState<IForm>(getData('user', {}));
   
   useEffect(() => {
     if (!getData('cart', null)) {
       setData('cart', []);
-    }
+    };
+    if (!getData('user', null)) {
+      setData('user', {});
+    };
   }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
   useEffect(() => {
     setData('cart', cart);
   }, [cart]);
 
+  useEffect(() => {
+    setData('user', user);
+  }, [user]);
+
   return (
     <MyGlobalContext.Provider value={{cart, setCart, user, setUser}}>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path='/register' element={<Register />}/>
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path='/register' element={<Register />}/>
+      </Routes>
+      <Footer />
     </MyGlobalContext.Provider>
   );
 }

af539336cdb985c8b4712fd44e02b42f6688ba06
quoclong01 <ngodacquoclong@gmail.com>
2022-07-05 16:22:42 +0700

    Fix intialState of cart reducer

--- src/app/pages/Cart/cart.reducer.ts
+++ src/app/pages/Cart/cart.reducer.ts
@@ -4,8 +4,7 @@ import { IActionReducer, IStateReducer } from '../../shared/interfaces/reducer';
 import { getData, setData } from './../../shared/common/common';
 
 const intitalStateCart = {
-  cart: getData('cart', []),
-  setCart: (cart: ICart) => cart,
+  cart: getData('cart', [])
 };
 
 const cartReducer = (
@@ -17,36 +16,36 @@ const cartReducer = (
     case TYPES.INCREASE_QUANTITY_CART:
       cartIndex = state.cart.findIndex((item) => item.id === action.payload.id);
       state.cart[cartIndex].quantity += 1;
+      setData('cart', [...state.cart]);
       return {
         ...state,
         cart: [...state.cart],
-        setCart: setData('cart', [...state.cart]),
       };
     case TYPES.DECREASE_QUANTITY_CART:
       cartIndex = state.cart.findIndex((item) => item.id === action.payload.id);
       if (state.cart[cartIndex].quantity > 1) {
         state.cart[cartIndex].quantity -= 1;
+        setData('cart', [...state.cart]);
         return {
           ...state,
           cart: [...state.cart],
-          setCart: setData('cart', [...state.cart]),
         };
       } else {
         const newCart = state.cart.filter(
           (item) => item.id !== action.payload.id
         );
+        setData('cart', newCart);
         return {
           ...state,
           cart: newCart,
-          setCart: setData('cart', newCart),
         };
       };
     case TYPES.DELETE_CART:
       const newCart = state.cart.filter((item) => item.id !== action.payload);
+      setData('cart', newCart);
       return {
         ...state,
         cart: newCart,
-        setCart: setData('cart', newCart),
       };
     case TYPES.ADD_CART:
       cartIndex = state.cart.findIndex(
@@ -57,19 +56,19 @@ const cartReducer = (
           ...action.payload,
           quantity: 1,
         };
+        setData('cart', [...state.cart, newCart]);
         return {
           ...state,
           cart: [...state.cart, newCart],
-          setCart: setData('cart', [...state.cart, newCart]),
         };
       } else {
         state.cart[cartIndex].quantity += 1;
+        setData('cart', [...state.cart]);
         return {
           ...state,
           cart: [...state.cart],
-          setCart: setData('cart', [...state.cart]),
         };
-      }
+      };
     default:
       return state;
   }
--- src/app/shared/interfaces/reducer.ts
+++ src/app/shared/interfaces/reducer.ts
@@ -2,7 +2,6 @@ import { ICart } from './cart';
 
 export interface IStateReducer {
   cart: ICart[];
-  setCart: (cart: ICart) => void
 };
 
 export interface IActionReducer {

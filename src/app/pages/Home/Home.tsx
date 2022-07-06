ec54389bad944854970b0a6afe48184f40cda611
quoclong01 <ngodacquoclong@gmail.com>
2022-07-06 08:15:50 +0700

    fix loading

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -8,23 +8,28 @@ import SectionContact from './partials/SectionContact';
 import { IProduct } from '../../shared/interfaces/product';
 const Home = () => {
   const [productList, setProductList] = useState<IProduct[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     axios
-    .get('https://6088e20da6f4a300174271e7.mockapi.io/products')
-    .then((response) => {
-      if(response) {
-        setProductList(response.data);
+      .get('https://6088e20da6f4a300174271e7.mockapi.io/products')
+      .then((response) => {
+        if (response) {
+          setProductList(response.data);
+        }
+        setLoading(false);
+      })
+      .catch((error) => {
         setLoading(true);
-      }
-    })
-    .catch((error) => {
-      setLoading(false);
-    });
+      });
   }, []);
 
   return loading ? (
+    <main className="loading-container">
+      <p className="spinner-text">loading...</p>
+      <div className="spinner"></div>
+    </main>
+  ) : (
     <main className="home-page">
       <SectionBanner />
       <SectionAbout />
@@ -36,13 +41,6 @@ const Home = () => {
       <SectionChooseus />
       <SectionProduct productList={productList} title="Products in today" />
       <SectionContact />
-    </main>
-  ) : (
-    <main className="loading-container">
-        <p className="spinner-text">
-            loading...
-        </p>
-        <div className="spinner"></div>
     </main>
   );
 };
